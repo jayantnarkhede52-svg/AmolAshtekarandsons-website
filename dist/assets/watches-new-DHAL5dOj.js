86c31746037a9691da5f9f9a3560f73faad74185
Jayant Narkhede <jayant.narkhede52@gmail.com>
2026-05-04 11:30:57 +0530

    Build and update dist folder

---
+++ dist/assets/watches-new-DHAL5dOj.js
@@ -0,0 +1,1 @@
+const s="/assets/silver-D2aSm-PO.webp",e="/assets/watches-new-RulpLvOm.jpg";export{s,e as w};
--- dist/assets/watches-new-DUr2QT-C.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/silver-CQpm69W9.png",e="/assets/watches-new-RulpLvOm.jpg";export{s,e as w};
